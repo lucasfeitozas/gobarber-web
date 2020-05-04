638b002294bd85f15fbc6be74175f2ccadcbd0f3
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-05-03 22:04:58 -0300

    criacao de arquivo para api de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import SignIn from './pages/SignIn/index';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Lucas' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
