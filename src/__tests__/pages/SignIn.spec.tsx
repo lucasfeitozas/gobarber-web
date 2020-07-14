dd247f65906aebe06815b298f8b64d520bf822da
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-07-13 22:24:39 -0300

    testes reactjs - SignIn using jest

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+// mock functions
+jest.mock('../../hooks/auth', () => {
+  return {
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+
+    fireEvent.click(buttonElement);
+
+    // wait - function used with async methods
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+
+    // debug();
+  });
+});
